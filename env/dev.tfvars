95612059cddf8aac60e1d97a9e925488340524ea
eswar <eswar.awsdevops@gmail.com>
2022-12-31 16:12:44 +0530

    updating ./roboshop-infra

---
+++ env/dev.tfvars
@@ -0,0 +1,15 @@
+instances = [
+  "frontend",
+  "cart",
+  "catalogue",
+  "user",
+  "payment",
+  "mongodb",
+  "rabbitmq",
+  "redis",
+  "shipping",
+  "mysql",
+
+
+
+]
---
+++ main.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "ec2" {
+  count = length(var.instances)
+  ami = data.aws.centos8_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  tags = {
+    Name = element(var.instances, count.index)
+  }
+}
+
+data "aws_ami" "centos8" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-0eb06c62785facb36"
+
+  ingress {
+    description = "ALL"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
