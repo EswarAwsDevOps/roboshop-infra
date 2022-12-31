e234265f9b60ea73064aa6dc936955faa44b8e85
eswar <eswar.awsdevops@gmail.com>
2022-12-31 16:16:56 +0530

    roboshop-infra

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2" {
   count = length(var.instances)
-  ami = data.aws.centos8.id
+  ami = data.aws_ami.centos8.id
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   tags = {
