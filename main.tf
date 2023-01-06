011ebc35d5088b7b1f2ad8e7408df84ae4b397c2
eswar <eswar.awsdevops@gmail.com>
2023-01-06 20:00:44 +0530

    creating DocDb

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-  module "vpc" {
+module "vpc" {
   source         = "github.com/EswarAwsDevOps/tf-module-vpc"
   env            = var.env
   default_vpc_id = var.default_vpc_id
@@ -13,13 +13,15 @@
   subnet_azs                 = each.value.subnet_azs
 }
 
-  module "docdb" {
-  source = "github.com/EswarAwsDevOps/tf-module-docdb"
-  env            = var.env
+
+module "docdb" {
+  source                  = "github.com/EswarAwsDevOps/tf-module-docdb"
+  env                     = var.env
+
   for_each                = var.docdb
   engine                  = each.value.engine
   backup_retention_period = each.value.backup_retention_period
-  preferred_backup_window = each.value.backup_retention_period
+  preferred_backup_window = each.value.preferred_backup_window
   skip_final_snapshot     = each.value.skip_final_snapshot
 
   vpc = module.vpc
