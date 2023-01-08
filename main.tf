ceb13186c8dfb2de85c79c4437977fd10d1b7cef
eswar <eswar.awsdevops@gmail.com>
2023-01-08 12:21:37 +0530

    creating rds

--- main.tf
+++ main.tf
@@ -57,14 +57,13 @@ module "rds" {
 module "elasticache" {
   source                  = "github.com/EswarAwsDevOps/tf-module-elasticache"
   env                     = var.env
-  kms_key_id              = var.kms_key_id
+
 
   for_each                = var.rds
   engine                  = each.value.engine
   engine_version          = each.value.engine_version
   node_type               = each.value.node_type
   num_cache_nodes         = each.value.num_cache_nodes
-  instance_class          = each.value.instance_class
 
   vpc = module.vpc
 }
